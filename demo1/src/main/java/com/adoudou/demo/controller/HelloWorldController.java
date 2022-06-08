bbe9b9f08b4864cb3e23ea4ee1f92192433c717b
adoudou <zhoujing_1@foxmail.com>
2022-06-08 16:31:33 +0800

    init

---
+++ demo1/src/main/java/com/adoudou/demo/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.adoudou.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello world";
+    }
+
+    @GetMapping("/hi")
+    public String getHi(){
+        return "hi world";
+    }
+}
---
+++ demo1/src/test/java/com/adoudou/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoudou.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
